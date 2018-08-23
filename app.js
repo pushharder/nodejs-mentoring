cd0c5332f1249b38b61eeffa22d203aa713c3833
Alexander Milovanov <alexander_milovanov@epam.com>
2018-08-23 21:52:16 +0300

    add event emitter

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 import config from './config/config.json';
-import {User, Product} from './models'
+import {User, Product} from './models';
+import events from 'events';
+
+class MyEmitter extends events {};
+const myEmitter = new MyEmitter();
+
+myEmitter.addListener('event', () => console.log('event happened'));
+
+myEmitter.emit('event');
+
 
 let user = new User();
 let product = new Product;
-console.log(config);
+// console.log(config);
+console.log(myEmitter.eventNames());
